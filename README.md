cf54dbcc823d93145c9ff2db64333690a4725898
Usama Kaleem <usamakaleem505@gmail.com>
2024-04-09 02:08:36 +0500

    react test query

--- README.md
+++ README.md
@@ -4,4 +4,24 @@
 write your test before actual implementation
 
 - Watch mode
+- filter options
+- describe (name, fn)
+. for grouping tests
 
+- Filename conventions
+. Files with .test.js or .test.jsx suffix
+. Files with .spec.js or .spec.jsx suffix
+. Files with .js or .tsx suffix in __test__ folder 
+
+- Coverage
+- Assertions
+
+-Test
+- Test component renders
+- Test component with props
+- Test component renders in different states
+- Test component react to event
+
+- RTL (React Testing Library ) 
+- Queries
+- getByRole
--- src/tests/FirstTest.test.tsx
+++ src/tests/FirstTest.test.tsx
@@ -1,16 +1,15 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { FirstTest } from '../components/FirstTest';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import { FirstTest } from "../components/FirstTest";
 
-test('First Test', () => {
+test("First Test", () => {
   render(<FirstTest />);
   const linkElement = screen.getByText(/Hello/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('Check props value', () => {
-    render(<FirstTest name="Usama"/>);
-    const linkElement = screen.getByText(/Hello Usama/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-  
+test("Check props valuess", () => {
+  render(<FirstTest name="Usama" />);
+  const linkElement = screen.getByText(/Hello Usama/i);
+  expect(linkElement).toBeInTheDocument();
+});
